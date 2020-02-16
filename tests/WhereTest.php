52858166c25ba6c481377c0cd0b1e06d542b6293
Dictor <kimdictor@gmail.com>
2020-02-16 14:12:04 +0000

    Intergrate like test into where test

--- tests/WhereTest.php
+++ tests/WhereTest.php
@@ -15,8 +15,9 @@ class WhereTest extends TestCase {
 	public function setUp() {
 	    $this->load_db();
 	    
-	    $names = ['hamster', 'chinchilla', 'dog', 'cat', 'rat', 'chamaeleon', 'turtle', 'chupacabra', 'catoblepas', 'catoblepas'];
-	    $kinds = ['rodentia', 'rodentia', 'canivora', 'carnivora', 'rodentia', 'squamata', 'testudines', null, null, 'game-character'];
+	    $names = ['hamster',	'chinchilla',	'dog',		'cat',		'rat',		'chamaeleon',	'turtle',		'chupacabra',	'catoblepas',	'catoblepas'];
+	    $kinds = ['rodentia',	'rodentia',		'canivora',	'carnivora','rodentia',	'squamata',		'testudines',	null,			null,			'game-character'];
+	    // age =	0				1				2			0			1			2				0				1				2				0
 	    
 	    for ($i = 0; $i < count($names); $i++) {
 	        $this->db->insert( 'pets', [
@@ -72,6 +73,59 @@ class WhereTest extends TestCase {
 		$this->assertCount(1, $result);
 		$this->assertEquals('catoblepas', $result[ 0 ][ 'name' ] );
 		$this->assertSame( null, $result[ 0 ][ 'kind' ] );
+	}
+	
+	
+	public function testWhereLike() : void {
+		$result = ( $this->db->select( '*' )
+				->from( 'pets' )
+				->where([ 'name' => JSONDB::like("at") ])
+				->get()
+			);
+		$this->assertCount(4, $result);
+		
+		$result = ( $this->db->select( '*' )
+				->from( 'pets' )
+				->where([ 'kind' => JSONDB::like("tia") ])
+				->get()
+			);
+		$this->assertCount(3, $result);
+	}
+	
+	public function testWhereLikeOr() : void {
+	    $result = ( $this->db->select( '*' )
+				->from( 'pets' )
+				->where([ 
+				    'name' => JSONDB::like("at"),
+				    'kind' => JSONDB::like("tia"),
+				    'age' => 2
+				])
+				->get()
+			);
+		$this->assertCount(8, $result);
+	}
+	
+	public function testWhereLikeAnd() : void {
+		$result = ( $this->db->select( '*' )
+				->from( 'pets' )
+				->where([ 
+				    'name' => JSONDB::like("at"),
+				    'kind' => JSONDB::like("tia")
+				], 'AND')
+				->get()
+			);
+		$this->assertCount(1, $result);
+		
+		$result = ( $this->db->select( '*' )
+				->from( 'pets' )
+				->where([ 
+				    'name' => JSONDB::like("at"),
+				    'kind' => JSONDB::like("tia"),
+				    'age' => 0
+				], 'AND')
+				->get()
+			);
+		$this->assertEmpty($result);
 	}
 	
 }
