83477a63c63e3b0d02e777a188b0bb55a8eb0ecc
Tomas Votruba <tomas.vot@gmail.com>
2022-02-04 17:47:28 +0100

    bump to PHPUnit 9.5, as supports PHP 7.3+

--- tests/WhereTest.php
+++ tests/WhereTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 declare( strict_types=1 );
 
 use PHPUnit\Framework\TestCase;
@@ -10,14 +10,14 @@ class WhereTest extends TestCase {
 	public function load_db() {
 		$this->db = new JSONDB( __DIR__ );
 	}
-	
+
 	// Both 'setUp' and 'tearDown' function is called by phpunit per every test function before test function is called
-	public function setUp() {
+	public function setUp(): void {
 	    $this->load_db();
-	    
+
 	    $names = ['hamster', 'chinchilla', 'dog', 'cat', 'rat', 'chamaeleon', 'turtle', 'chupacabra', 'catoblepas', 'catoblepas'];
 	    $kinds = ['rodentia', 'rodentia', 'canivora', 'carnivora', 'rodentia', 'squamata', 'testudines', null, null, 'game-character'];
-	    
+
 	    for ($i = 0; $i < count($names); $i++) {
 	        $this->db->insert( 'pets', [
     			'name' => $names[$i],
@@ -26,33 +26,33 @@ class WhereTest extends TestCase {
     		]);
 	    }
 	}
-	
-	public function tearDown() {
+
+	public function tearDown(): void {
 	    $this->db->delete()
 			->from( 'pets' )
 			->trigger();
 	}
-	
+
 	public function testWhereOr() {
 	    $result = ( $this->db->select( '*' )
 				->from( 'pets' )
-				->where([ 
+				->where([
 				    'kind' => 'rodentia',
 				    'age' => 0
 				])->get()
 			);
 		$this->assertCount(6, $result);
-		
+
 		$result = ( $this->db->select( '*' )
 				->from( 'pets' )
-				->where([ 
+				->where([
 				    'kind' => 'squamata',
 				    'age' => 2
 				])->get()
 			);
 		$this->assertCount(3, $result);
 	}
-	
+
 	public function testWhereNullOr() {
 		$result = ( $this->db->select( '*' )
 				->from( 'pets' )
@@ -60,11 +60,11 @@ class WhereTest extends TestCase {
 			);
 		$this->assertCount(2, $result);
 	}
-	
+
 	public function testWhereNullAnd() {
 		$result = ( $this->db->select( '*' )
 				->from( 'pets' )
-				->where([ 
+				->where([
 					'name' => 'catoblepas',
 					'kind' => null
 				], 'AND')->get()
@@ -73,6 +73,6 @@ class WhereTest extends TestCase {
 		$this->assertEquals('catoblepas', $result[ 0 ][ 'name' ] );
 		$this->assertSame( null, $result[ 0 ][ 'kind' ] );
 	}
-	
+
 }
 ?>
