aa366218e8276803dc3c1c054671112d7453efe0
James John <me@donjajo.com>
2022-02-06 19:23:21 +0000

    Adopt PSR 12 coding standard

--- ecs.php
+++ ecs.php
@@ -21,8 +21,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ]]);
 
     // run and fix, one by one
-    // $containerConfigurator->import(SetList::SPACES);
-    // $containerConfigurator->import(SetList::ARRAY);
+    $containerConfigurator->import(SetList::SPACES);
+    $containerConfigurator->import(SetList::ARRAY);
     // $containerConfigurator->import(SetList::DOCBLOCK);
-    // $containerConfigurator->import(SetList::PSR_12);
+    $containerConfigurator->import(SetList::PSR_12);
 };
--- tests/PerformanceTest.php
+++ tests/PerformanceTest.php
@@ -1,39 +1,44 @@
 <?php
-declare( strict_types = 1 );
 
+declare(strict_types=1);
+
+use Jajo\JSONDB;
 use PHPUnit\Framework\TestCase;
-use \Jajo\JSONDB;
 
-class PerformanceTest extends TestCase {
-	private $jsondb;
+class PerformanceTest extends TestCase
+{
+    private $jsondb;
 
-	protected function setUp(): void {
-		$this->jsondb = new JSONDB(__DIR__, JSON_UNESCAPED_UNICODE);
-	}
+    protected function setUp(): void
+    {
+        $this->jsondb = new JSONDB(__DIR__, JSON_UNESCAPED_UNICODE);
+    }
 
-	protected function tearDown(): void {
-		unlink( __DIR__ . '/food.json' );
-	}
+    protected function tearDown(): void
+    {
+        unlink(__DIR__ . '/food.json');
+    }
 
-	public function testInsert(): void {
-		$i = 0;
-		while ( $i < 5000 ) {
-			$sum = 0;
-			for ($j = 0; $j < 1000; $j++ ) {
-				$start = hrtime( true );
-				$this->jsondb->insert( 'food', [
-					'name' => 'Rice',
-					'class' => 'Carbohydrate',
-				] );
-				$stop = hrtime( true );
-				$sum += ( $stop - $start )/1000000;
-			}
-			$i += $j;
-			fprintf( STDOUT, "\nTook average of %fms to insert 1000 records - BATCH %d", $sum, $i / 1000 );
-			fflush(STDOUT);
-		}
+    public function testInsert(): void
+    {
+        $i = 0;
+        while ($i < 5000) {
+            $sum = 0;
+            for ($j = 0; $j < 1000; $j++) {
+                $start = hrtime(true);
+                $this->jsondb->insert('food', [
+                    'name' => 'Rice',
+                    'class' => 'Carbohydrate',
+                ]);
+                $stop = hrtime(true);
+                $sum += ($stop - $start) / 1000000;
+            }
+            $i += $j;
+            fprintf(STDOUT, "\nTook average of %fms to insert 1000 records - BATCH %d", $sum, $i / 1000);
+            fflush(STDOUT);
+        }
 
-		$foods = $this->jsondb->select('name')->from( 'food' )->get();
-		$this->assertCount( 5000, $foods );
-	}
+        $foods = $this->jsondb->select('name')->from('food')->get();
+        $this->assertCount(5000, $foods);
+    }
 }
